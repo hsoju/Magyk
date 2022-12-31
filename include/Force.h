44c0c2494a2344f5075b697c40e9487243daa7cb
hsoju <78128765+hsoju@users.noreply.github.com>
2022-12-31 14:52:47 -0500

    update scope

--- include/Force.h
+++ include/Force.h
@@ -15,38 +15,29 @@ namespace Magyk
 			return &forceInterface;
 		}
 
-		static inline bool can_hover = false;
-		static inline bool is_hovering = false;
-		static inline bool increasing = false;
+		bool can_hover = false;
+		bool is_hovering = false;
+		bool increasing = false;
 
-		static inline bool facing_down = false;
-		static inline bool has_jumped = false;
+		bool facing_down = false;
+		bool has_jumped = false;
 
-		static inline uint32_t jump_cycle = 0;
-		static inline uint32_t jump_window = 25;
+		uint32_t jump_cycle = 0;
+		uint32_t jump_window = 25;
 
-		static inline uint32_t facing_cycle = 0;
-		static inline uint32_t facing_window = 50;
+		uint32_t facing_cycle = 0;
+		uint32_t facing_window = 50;
 
-		static inline float max_height = 18.0f;
-		static inline float	drag = 0.0f;
+		float max_height = 18.0f;
+		float drag = 0.0f;
 
-		static inline bool r_cast_out = false;
-		static inline bool l_cast_out = false;
+		bool r_cast_out = false;
+		bool l_cast_out = false;
 
 		const RE::BSFixedString r_cast = RE::BSFixedString("bWantCastRight");
 		const RE::BSFixedString l_cast = RE::BSFixedString("bWantCastLeft");
  
-		static void SetMaxHeight();
-
-		static uint32_t RadianRange(float a_radian);
-		static float RadiansToDegrees(float a_radian);
-		static bool CheckDirection(bool use_axis=false);
-
-		static void IncreaseElevation(RE::bhkCharacterController* a_controller, float height);
-		static void DampenFall(RE::bhkCharacterController* a_controller);
-
-		void CheckConditions(RE::bhkCharacterController* a_controller);
+		void SetMaxHeight();
 		void Update(RE::Actor* a_actor);
 
 	protected:
@@ -65,5 +56,15 @@ namespace Magyk
 				stl::write_vfunc<RE::PlayerCharacter, 0xAD, PlayerCharacter_Update>();
 			}
 		};
+
+	private:
+		uint32_t RadianRange(float a_radian);
+		float    RadiansToDegrees(float a_radian);
+		bool     CheckDirection(bool use_axis = false);
+
+		void IncreaseElevation(RE::bhkCharacterController* a_controller, float height);
+		void DampenFall(RE::bhkCharacterController* a_controller);
+
+		void CheckConditions(RE::bhkCharacterController* a_controller);
 	};
 }
